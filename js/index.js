37f3f4fc613d665653c4f820a10e674b1c8e0831
yunmee0704 <900704630c@gmail.com>
2022-03-16 22:47:31 +0900

    final

--- js/index.js
+++ js/index.js
@@ -1,4 +1,44 @@
 $(function () {
+  
+  $('.list>li .sub_list').slideUp();
+
+function stagger() { //함수로 만듬
+  
+    $('.brand_slide.on .swiper-slide-active .list>li').each(function (index, item) { // 각각 실행
+        setTimeout(function () { 
+            $('.brand_slide.on .swiper-slide-active .list>li').eq(index).find('.sub_list').slideDown(500).delay(1000).slideUp(500);
+             //총모션 2초
+        }, 2000 * index) //2초*순서 뒤에 실행해서 순차실행
+       
+      
+    })
+    
+};
+// stagger();
+
+
+//   setInterval(function () {
+//     stagger();
+   
+// }, 10000) //3개니 6초
+
+ //ranking 영역 버튼
+ $('.sc_ranking .tap_wrap > div').click(function(e){
+  e.preventDefault();
+  $(this).addClass('on').siblings().removeClass('on');
+  target = $(this).attr('href');
+    $(target).addClass('on').siblings().removeClass('on')
+    setInterval(function () {
+      stagger();
+     
+  }, 10000)
+
+})
+
+
+
+
+
   let gnb = new Swiper("header .gnb", {
     slidesPerView: "auto",
     freeMode: true,
@@ -27,16 +67,37 @@ $(function () {
 
 
   });
-//WHAT'S NEW
+//WHAT'S NEW , SPECIAL PRICE
   let slide03 = new Swiper(".slide03", {
-    slidesPerView: "auto",
-    spaceBetween: 8,
+    slidesPerView: "2.8",
+    spaceBetween: 5,
 
+
+  });
+  let sp_price = new Swiper(".sp_price", {
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
 
   });
 
   //MD' PICK GNB
   let md_pick_gnb = new Swiper(".md_pick .gnb", {
+    slidesPerView: "auto",
+    spaceBetween: 24,
+ 
+
+  });
+  let sc_ranking_gnb = new Swiper(".sc_ranking .gnb", {
     slidesPerView: "auto",
     spaceBetween: 24,
 
@@ -56,7 +117,7 @@ $(function () {
 
 
   });
-
+//md_pick menubar 
   $('.sc_md_pick .gnb li').click(function(e){
     e.preventDefault();
     $(this).addClass('on').siblings().removeClass('on');
@@ -82,11 +143,42 @@ $(function () {
 
   });
 
-  let grid_slide = new Swiper(".grid_slide", {
+  let grid_slide2 = new Swiper(".grid_slide2", {
+    slidesPerView: "auto",
+    spaceBetween: 19.5,
     pagination: {
       el: ".swiper-pagination",
     },
   });
+  let grid_slide = new Swiper(".grid_slide", {
+    slidesPerView: "auto",
+    spaceBetween: 19.5,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  let brand_slide = new Swiper(".brand_slide", {
+    slidesPerView: "auto",
+    spaceBetween: 19.5,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+  //ranking 메뉴바
+  });
+  $('.sc_ranking nav .gnb .swiper-wrapper>div').click(function(e){
+    e.preventDefault();
+    $(this).addClass('on').siblings().removeClass('on');
+    leftVal = $(this).position().left;
+    wdVal =$(this).children().outerWidth();
+    $('.sc_ranking .bar').css({left:leftVal,width:wdVal})
+  })
+
+
+
+
+
 
 
   let slide07 = new Swiper(".slide07", {
@@ -94,11 +186,15 @@ $(function () {
     slidesPerView: 1.5,
     loop: true,
     spaceBetween: 5,
-    // // slidesPerGroup: 3,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
+   
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     // // loopFillGroupWithBlank: true,
 
   });
@@ -190,7 +286,7 @@ $('.special_product ul li').click(function(e){
   e.preventDefault();
   $(this).addClass('on').siblings().removeClass('on');
   leftVal = $(this).position().left;
-  wdVal =$(this).children().outerWidth();
+  wdVal =$(this).outerWidth();
   $('.special_product .bar').css({left:leftVal,width:wdVal})
 })
 
@@ -210,5 +306,24 @@ $('.special_product ul li').click(function(e){
     $(target).addClass('on').siblings().removeClass('on')
   }
 })
+$(window).scroll(function(){
+  let scrT= $(window).scrollTop();
+  console.log(scrT);
+  if(scrT>0){
+    $('header').css({top:'-50px'})
+    $('.float_menu').css({bottom:'-90px'})
 
+   
+  }else{
+    
+    $('header').css({top:'0'})
+    $('.float_menu').css({bottom:'0'})
+  }
+    
+  if(scrT > 14000){
+    $('.float_menu').css({bottom:'0'})
+  }  
+    
+  
+})
 });//지우지마세요.
